f732c47eccae9c419f15f5c3a02f7a13ef0d6116
kji-dec <dec22sag@gmail.com>
2023-01-19 11:08:36 +0900

    feat: Create CRUD using mixins
    by using mixins, pagination, and serializer, created CRUD functions

--- shinhanrest/product/views.py
+++ shinhanrest/product/views.py
@@ -1,120 +1,54 @@
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, mixins
 from .models import Product
+from .serializers import ProductSerializer
+from .paginations import ProductLargePagination
 
-class ProductListView(APIView):
-    def post(self, request, *args, **kwargs):
-        #lecturer's code
-        name=request.data.get('name')
-        price=request.data.get('price')
-        product_type=request.data.get('product_type')
 
-        product = Product(
-            name=name,
-            price=price,
-            product_type=product_type,
-        )
+class ProductListView(
+    mixins.ListModelMixin, 
+    mixins.CreateModelMixin,
+    generics.GenericAPIView,
+):
+    serializer_class = ProductSerializer
+    # pagination_class = ProductLargePagination
 
-        product.save()
-
-        return Response({
-            'id': product.id,
-            'name': product.name,
-            'price': product.price,
-            'type': product.product_type,
-        }, status=status.HTTP_201_CREATED)
-
-        # my code
-        # product = Product(
-        #     name=request.data.get('name'),
-        #     price=request.data.get('price'),
-        #     product_type=request.data.get('product_type'),
-        # )
-        # product.save()
-        # return Response({"id": product.id}, status=status.HTTP_201_CREATED)
-
-    def get(self, request, *args, **kwargs):
+    def get_queryset(self):
         products = Product.objects.all()
+        price = self.request.query_params.get('price')
+        name = self.request.query_params.get('name')
 
-        res_list = []
-
-        if 'price' in request.query_params:
-            price = request.query_params['price']
+        if price:
             products = products.filter(price__lte=price)
         
-        if 'name' in request.query_params:
-            name = request.query_params['name']
+        if name:
             products = products.filter(name__contains=name)
         
-        products = products.order_by('id')
-
-        for product in products:
-            res = {
-                'id': product.id,
-                'name': product.name,
-                'price': product.price,
-                'type': product.product_type,
-            }
-            res_list.append(res)
         
-        return Response(res_list)
-    
+        return products.order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, args, kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, args, kwargs)
 
 
-class ProductDetailView(APIView):
-    def put(self, request, pk, *args, **kwargs):
+class ProductDetailView(
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    generics.GenericAPIView,
+):
+    serializer_class = ProductSerializer
 
-        try:
-            product = Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_queryset(self):
+        return Product.objects.all().order_by('id')
 
-        # lecturer's code with dirty flag
-        # 아예 데이터를 수정하지 않는 경우에 대해서는 save를 하지 않아도 되니까
-        # dirty flag를 이용해 save 자원을 낭비하지 않을 수 있음
-        # dirty 값을 사용하지 않아도 ㄱㅊ은편
-        dirty = False
-        for field, value in request.data.items():
-            if field not in [f.name for f in product._meta.get_fields()]:
-                continue
-            dirty = dirty or (value != getattr(product, field))         
-            setattr(product, field, value)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, args, kwargs)
 
-        if dirty:
-            product.save()
-        
-        # my code
-        # name=request.data.get('name', product.name)
-        # price=request.data.get('price', product.price)
-        # product_type=request.data.get('product_type', product.product_type)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, args, kwargs)
 
-        # product.name = name
-        # product.price = price
-        # product.product_type = product_type
-
-        # product.save()
-
-        return Response()
-
-    def delete(self, request, pk, *args, **kwargs):
-        if Product.objects.filter(pk=pk).exists():
-            Product.objects.get(pk=pk).delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def get(self, request, pk, *args, **kwargs):
-
-        try:
-            product = Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        res = {
-            'name': product.name,
-            'price': product.price,
-            'type': product.product_type,
-        }
-        
-        return Response(res)
+    def put(self, request, *args, **kwargs):
+        return self.partial_update(request, args, kwargs)
