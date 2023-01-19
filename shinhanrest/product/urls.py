a08cb48f5e2489be85919b5719d5cf56a899c52b
kji-dec <dec22sag@gmail.com>
2023-01-19 16:24:49 +0900

    feat: Create filtering comment function
    create filtering function using product_id

--- shinhanrest/product/urls.py
+++ shinhanrest/product/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("/<int:pk>", views.ProductDetailView.as_view()),
     path("", views.ProductListView.as_view()),
-    path("/comment", views.CommentListView.as_view()),
+    path("/<int:product_id>/comment", views.CommentListView.as_view()),
 ]
