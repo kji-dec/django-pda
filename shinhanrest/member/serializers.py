644e4af4f943701db85a5c67ec98155a5d2eb970
kji-dec <dec22sag@gmail.com>
2023-01-25 10:46:35 +0900

    feat: Create password validation
    created password validation code with length validation
    plus, commented by code, and added lecturer's code

--- shinhanrest/member/serializers.py
+++ shinhanrest/member/serializers.py
@@ -3,16 +3,23 @@ from django.contrib.auth.hashers import make_password
 
 from .models import Member
 
-class MemberSerializer(serializers.ModelSerializer):    
-    def create(self, validated_data):
-        member = Member.objects.create(
-            username=validated_data['username'],
-            password=make_password(validated_data['password']),
-            tel = validated_data['tel'],
-        )
-        member.save()
+class MemberSerializer(serializers.ModelSerializer):
+    #lecturer's code
+    def validate_password(self, value):
+        # validation이 끝난 value를 반환해줌
+        if len(value) < 8:
+            raise serializers.ValidationError('Too short password')
+        return make_password(value)
+    # my code
+    # def create(self, validated_data):
+    #     member = Member.objects.create(
+    #         username=validated_data['username'],
+    #         password=make_password(validated_data['password']),
+    #         tel = validated_data['tel'],
+    #     )
+    #     member.save()
 
-        return member
+    #     return member
     
     class Meta:
         model = Member
